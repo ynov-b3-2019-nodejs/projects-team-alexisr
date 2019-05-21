229a767997b6e78e2674230b5e700ec2675e54cf
Alexis Rieucau <alexis.rieucau@gmail.com>
2019-05-22 01:23:49 +0200

    add login interface and session handling

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const cookeParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const expressSession = require('express-session');
+
+const app = express();
+const server = require('http').createServer(app);
+const user = require('./routes/user.route');
+
+const io = require('socket.io').listen(server)
+const ios = require('socket.io-express-session')
+
+let session = expressSession({
+    secret: 'alexis',
+    resave: true,
+    saveUninitialized: true
+});
+
+io.use(ios(session));
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookeParser());
+app.use(session);
+app.use(expressValidator());
+
+app.use('/user', user);
+
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.send('Vous êtes à l\'accueil, que puis-je pour vous ?');
+});
+
+app.get('/compter/:nombre', function(req, res) {
+    if(!isNaN(req.params.nombre) && req.params.nombre > 0) {
+        let tNoms = ['Alexis', 'Bob', 'Peter'];
+        let a = tNoms.length;
+        res.render('compter.ejs', {nombre: req.params.nombre, noms: tNoms});
+    } else {
+        res.setHeader('Content-Type', 'text/plain');
+        res.status(404).send('Page introuvable !');
+    }
+})
+
+app.use(function(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(404).send('Page introuvable !');
+});
+
+
+io.sockets.on('connection', function(socket) {
+    socket.emit('message', 'Vous etes bien connecté !');
+    socket.broadcast.emit('message', 'Un autre client vient de se connecter !');
+    console.log(socket.handshake.session);
+    console.log('socket : '+socket.id)
+
+    let session = socket.handshake.session;
+
+    if(!session.user) {
+        console.log('Vous n\'êtes pas connecté');
+    } else {
+        console.log('Connecté en tant que '+session.user.name);
+        session.user.socket = socket.id;
+    }
+    console.log(JSON.stringify(session.user));
+
+    socket.on('message', function(message) {
+        console.log('Un message du client : '+message);
+    });
+});
+
+server.listen(8080);
+
+/*server.on('close', function() {
+    console.log("Bye bye");
+})*/
